e0d585995d973718f96cfa0c322a6451bce4a394
Xiangrong Yang <xryang@thoughtworks.com>
2017-10-05 22:00:42 +0800

    Use stubs from remote repo.

--- src/test/java/org/amuji/service/GetProductTest.java
+++ src/test/java/org/amuji/service/GetProductTest.java
@@ -1,5 +1,6 @@
 package org.amuji.service;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,13 +8,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.*;
+
 /**
  * Xiangrong Yang
  * 04/10/2017
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
-@AutoConfigureStubRunner(ids = {"org.amuji:http-server-producer:+:stubs:6565"}, workOffline = true)
+@AutoConfigureStubRunner(ids = {"org.amuji:http-server-producer:+:stubs:6565"}, repositoryRoot = "http://localhost:8081/repository/maven-snapshots/")
 public class GetProductTest {
 
     @Autowired
@@ -21,6 +24,6 @@ public class GetProductTest {
     @Test
     public void getProductById() {
         String product = get.getProduct();
-        System.out.println(product);
+        assertNotNull(product);
     }
 }

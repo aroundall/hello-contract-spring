326c75ea40209e44035689fb9bd81322bb8308b6
Xiangrong Yang <xryang@thoughtworks.com>
2017-10-05 21:12:35 +0800

    Re-org files and add readme

--- src/main/java/org/amuji/hello/Application.java
+++ src/main/java/org/amuji/hello/Application.java
@@ -1,16 +1,15 @@
-package org.amuji.hellospringcontract;
+package org.amuji.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.contract.stubrunner.server.EnableStubRunnerServer;
-import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 
 @SpringBootApplication
 @EnableStubRunnerServer
 //@AutoConfigureStubRunner(ids = "org.amuji:http-server-producer:+:stubs:8095", workOffline = false)
-public class HelloSpringContractApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloSpringContractApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }

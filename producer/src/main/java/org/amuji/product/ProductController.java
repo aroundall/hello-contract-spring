f7206c4348051010618384ba6dff6aec1f958b1c
Xiangrong Yang <xryang@thoughtworks.com>
2017-10-13 23:05:49 +0800

    works - mock spring component

--- producer/src/main/java/org/amuji/product/ProductController.java
+++ producer/src/main/java/org/amuji/product/ProductController.java
@@ -1,5 +1,6 @@
 package org.amuji.product;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("products")
 public class ProductController {
+    @Autowired
+    ProductRepo repo;
+
     @GetMapping("{id}")
     public Product get(@PathVariable Long id) {
-        return new Product().setId(id).setName("Dummy Product");
+        return repo.get(id);
     }
 }

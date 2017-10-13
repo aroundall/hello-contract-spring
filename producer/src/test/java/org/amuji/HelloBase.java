0457935e18d6db923491d4b306848c60fa870697
Xiangrong Yang <xryang@thoughtworks.com>
2017-10-13 22:38:10 +0800

    use base package

--- producer/src/test/java/org/amuji/HelloBase.java
+++ producer/src/test/java/org/amuji/HelloBase.java
@@ -1,13 +1,14 @@
-package org.amuji.product;
+package org.amuji;
 
 import com.jayway.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.amuji.product.ProductController;
 import org.junit.Before;
 
 /**
  * Xiangrong Yang
  * 04/10/2017
  */
-public class MvcTest {
+public class HelloBase {
     @Before
     public void setup() {
         RestAssuredMockMvc.standaloneSetup(new ProductController());

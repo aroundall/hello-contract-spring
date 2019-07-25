7fe077d8e9879bf8a5d102763b10e7d90dc7e38b
Xiangrong Yang <yangxr000@gmail.com>
2019-07-26 01:11:23 +0800

    stub-runner application works

--- stub-runner/src/main/java/org/amuji/hello/Application.java
+++ stub-runner/src/main/java/org/amuji/hello/Application.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.contract.stubrunner.server.EnableStubRunnerServ
 
 @SpringBootApplication
 @EnableStubRunnerServer
-//@AutoConfigureStubRunner(ids = "org.amuji:http-server-producer:+:stubs:8095", workOffline = false)
 public class Application {
 
 	public static void main(String[] args) {
